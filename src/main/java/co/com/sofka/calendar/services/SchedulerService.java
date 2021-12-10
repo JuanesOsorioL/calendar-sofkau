0f3c4bfdcadfbacb367fbceab1ce7e5ee215c624
juanesosorio <Juanchis_1989@hotmail.com>
2021-12-10 18:24:29 -0500

    segundo TODO: debe pasarlo a reactivo, no puede trabaja elementos bloqueantes

--- src/main/java/co/com/sofka/calendar/services/SchedulerService.java
+++ src/main/java/co/com/sofka/calendar/services/SchedulerService.java
@@ -33,7 +33,8 @@ public class SchedulerService {
 
         //TODO: debe pasarlo a reactivo, no puede trabaja elementos bloqueantes
         //TODO: trabajar el map reactivo y no deben colectar
-        var program = programRepository.findById(programId).block();
+        //var program = programRepository.findById(programId).block();
+        var program = programRepository.findById(programId);
         return Optional.ofNullable(program)
                 .map(this::getDurationOf)
                 .orElseThrow(() -> new RuntimeException("El programa academnico no existe"))
